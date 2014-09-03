a9b1d9fd2a49cd30d13c45fd189902ada5c9defd
Igo Brilhante <brilhanteigo@gmail.com>
2014-09-03 18:50:05 +0200

    Adjusting parameters for hadoop

--- bin/tweets-per-state.sh
+++ bin/tweets-per-state.sh
@@ -4,16 +4,14 @@ BIN="hadoop"
 JAR="../target/scala-2.10/twitter-analysis-scalding-0.0.1.jar"
 JOBS="br.ufc.scalding.jobs"
 INPUT="/tweet-data/brasil-tweets-state.tsv"
-ESTADOS="/brasil-estados.tsv"
 OUTPUT="/output-brasil/"
 
 JOB="TweetsPerStateJob"
 
 ${BIN} jar ${JAR} \
 com.twitter.scalding.Tool \
--D mapred.output.compress=false  \
--D mapred.child.java.opts=-Xmx2048m \
 -D mapred.reduce.tasks=20 \
+-D mapred.min.split.size=2000000000 \
 ${JOBS}"."${JOB} \
 --hdfs \
 --input ${INPUT} \
